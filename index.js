edae3cb58c1e3ff69a9d46b0a002b5991716e610
Torsten Paul <Torsten.Paul@gmx.de>
2024-12-01 17:12:59 +0100

    Add 3mf of the day 1 christmas tree.

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
 setDay(1, { author: 'Torsten Paul', license: 'CC0', dir: 'Tree',
         img: 'Tree.png',
         src: 'Tree.scad',
-	info: '<p>Welcome to Scadvent 2024 - Tutorial Edition. Introduction will start tomorrow... till then here is a little christmas tree.</p>'
+	bin: 'Tree.3mf',
+	info: '<p>Welcome to Scadvent 2024 - Tutorial Edition. Introduction will start tomorrow... till then here is a little christmas tree.</p><p><small>The model is also available via <a href="https://www.printables.com/model/1093688-christmas-tree">Printables</a>.</small></p>'
 });
 setDay(22, { author: 'Jordan Brown', license: 'CC0', dir: 'Nessie',
         img: 'Nessie.png',
