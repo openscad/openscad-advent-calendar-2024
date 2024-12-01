5789f44ac96a37cf25b27b29f5560c2de1bcc3aa
Torsten Paul <Torsten.Paul@gmx.de>
2024-12-01 18:06:01 +0100

    Add designs to index.

--- index.js
+++ index.js
@@ -5,8 +5,22 @@ setDay(1, { author: 'Torsten Paul', license: 'CC0', dir: 'Tree',
 	bin: 'Tree.3mf',
 	info: '<p>Welcome to Scadvent 2024 - Tutorial Edition. Introduction will start tomorrow... till then here is a little christmas tree.</p><p><small>The model is also available via <a href="https://www.printables.com/model/1093688-christmas-tree">Printables</a>.</small></p>'
 });
+setDay(8, { author: 'Ulrich Bär', license: 'CC0', dir: 'Coaster',
+        img: 'Coaster.png',
+        src: 'Coaster.scad',
+	bin: 'Coaster.3mf'
+});
+setDay(15, { author: 'Ulrich Bär', license: 'CC0', dir: 'NightHawks',
+        img: 'NightHawks.png',
+        src: 'NightHawks.scad'
+});
 setDay(22, { author: 'Jordan Brown', license: 'CC0', dir: 'Nessie',
         img: 'Nessie.png',
         src: 'Nessie.scad',
 	info: '<p>Real, maybe.</p>'
+});
+setDay(23, { author: 'Stone Age Sculptor', license: 'CC0', dir: 'ChristmasA-Z',
+        img: 'ChristmasA-Z.gif',
+        src: 'ChristmasA-Z.scad',
+	info: '<p>See also thread on <a href="https://www.reddit.com/r/openscad/comments/1gr3xgb/working_towards_scadvent/">reddit</a>.</p>'
 });
